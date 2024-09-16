19343431b67c07aae7aca09e7c510638b197e949
Matthew Pont <matthew.pont@gmail.com>
2024-09-16 18:08:26 +0100

    Update cb_prices.php

--- page-templates/blocks/cb_prices.php
+++ page-templates/blocks/cb_prices.php
@@ -7,16 +7,16 @@
             the_row();
         ?>
                 <h2 style="break-after: avoid;"><?php echo get_sub_field('title'); ?></h2>
-                <div class="mb-5">
+                <div class="mb-5" style="break-after: column;">
                     <div class="mb-2"><?php echo get_sub_field('text'); ?></div>
                     <?php
                     if ( get_sub_field('cta') ) {
                     ?>
-                    <a href="<?php echo get_sub_field('cta'); ?>" class="btn btn-green mb-5" target="_blank">Book Now</a>
+                    <a href="<?php echo get_sub_field('cta'); ?>" class="btn btn-green" target="_blank">Book Now</a>
                     <?php
                     } else {
                     ?>
-                    <a href="https://www.phorest.com/salon/timetobeme" class="btn btn-green mb-5" target="_blank">Book Now</a>
+                    <a href="https://www.phorest.com/salon/timetobeme" class="btn btn-green" target="_blank">Book Now</a>
                     <?php
                     }
                     ?>
