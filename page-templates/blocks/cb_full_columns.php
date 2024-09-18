4d8b08e6d9da9b6161f15cf53eb4abfa2d0d1b8f
Matthew Pont <matthew.pont@gmail.com>
2024-09-18 10:13:19 +0100

    Update cb_full_columns.php

--- page-templates/blocks/cb_full_columns.php
+++ page-templates/blocks/cb_full_columns.php
@@ -6,6 +6,17 @@ $contentBg = get_field('background') == 'cream' ? 'bg-white' : 'bg-cream';
 ?>
 <section class="full_columns <?=$bg?> py-5">
     <div class="container-fluid">
+        <?php
+        if ( get_field('title') ) {
+        ?>
+        <div class="row g-4 align-items-start">
+            <div class="col-md">
+                <h2 class="text-center"><?=get_field('title')?></h2>
+            </div>
+        </div>
+        <?php
+        }
+        ?>
         <div class="row g-4 align-items-start">
 <?php
 if( have_rows('columns') ):
