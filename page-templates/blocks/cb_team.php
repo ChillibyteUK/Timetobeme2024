4cccac65fa1f36670a1cffa68766cb70a4a86baa
Matthew Pont <matthew.pont@gmail.com>
2024-09-18 17:14:09 +0100

    Update cb_team.php

--- page-templates/blocks/cb_team.php
+++ page-templates/blocks/cb_team.php
@@ -10,9 +10,9 @@ if( have_rows('team') ):
             <div class="col-12 col-lg-3 col-md-6 col-sm-6 d-flex flex-column align-self-stretch shadow">
                 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="img-fluid w-100" />
                 <div class="bg-white p-3 h-100">
-                    <div class="fw-bold"><?the_sub_field("title");?></div>
-                    <div class="fw-bold"><?the_sub_field("position");?></div>
-                    <div class=""><?the_sub_field("bio");?></div>
+                    <div class="fw-bold"><? echo wp_kses_post( get_sub_field("title") );?></div>
+                    <div class="fw-bold"><? echo wp_kses_post( get_sub_field("position") );?></div>
+                    <div class=""><? echo wp_kses_post( get_sub_field("bio") );?></div>
                 </div>
             </div>
         <?php
