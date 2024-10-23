b426bbc38bf382afd0dd67f743dffe78603f0c72
Matthew Pont <matthew.pont@gmail.com>
2024-10-23 12:29:14 +0100

    Update cb_awards.php

--- page-templates/blocks/cb_awards.php
+++ page-templates/blocks/cb_awards.php
@@ -10,26 +10,26 @@
         <div class="container-xl">
             <div class="row align-items-center gx-5">
                 <div class="col-lg-4 text-center">
-                    <img src="https://timetobeme.local/wp-content/uploads/2024/09/award-4.png" class="img-fluid">
+                    <img src="//wp-content/uploads/2024/09/award-4.png" class="img-fluid">
                     <div class="py-4">
                         <p>In April 2023 – TIME to be ME were awarded the prestigious Client Experience Award.</p>
                         <p>Awarded to salons who have maintained an average customer rating of four and five stars, from a minimum of 60 online reviews for 12 consecutive months ( 1st March 2022 – 28th February 2023).</p>
                     </div>
-                    <img src="https://timetobeme.local/wp-content/uploads/2024/09/award-1.png" class="img-fluid">
+                    <img src="//wp-content/uploads/2024/09/award-1.png" class="img-fluid">
                 </div>
                 <div class="col-lg-4 text-center">
-                    <img src="https://timetobeme.local/wp-content/uploads/2024/09/award-5.jpg" class="img-fluid">
+                    <img src="//wp-content/uploads/2024/09/award-5.jpg" class="img-fluid">
                     <div class="py-4">
                         <p>In 2021 TIME to be ME came finalist at the Professional Beauty Awards in London for Spa/Salon Team of the Year. On receiving the recognition. She said: ‘To be listed within the final few on our first time of entering is a huge achievement in itself and testament to the amazing team that we have at TIME to be ME.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 text-center">
-                    <img src="https://timetobeme.local/wp-content/uploads/2024/09/award-3.png" class="img-fluid">
+                    <img src="//wp-content/uploads/2024/09/award-3.png" class="img-fluid">
                     <div class="py-4">
                         <p>In 2020 TIME to be ME were awarded the Client Experience Award</p>
                         <p>The Phorest Client Experience Award has been awarded to only a select group of Phorest clients who have achieved the incredible rating of 4.5 (or higher) out-of-five across all reviews they have received since the feature launched in 2012.</p>
                     </div>
-                    <img src="https://timetobeme.local/wp-content/uploads/2024/09/award-2.png" class="img-fluid">
+                    <img src="//wp-content/uploads/2024/09/award-2.png" class="img-fluid">
                 </div>
             </div>
         </div>
